cfc74f2b13ef4ebdb1eb1dc4c147dc4f85762cb7
Bas van Leeuwen <bas.vanleeuwen@peterconnects.com>
2020-01-13 22:40:43 +0100

    Support RM4/Rails 5

--- lib/wiking_notified_users_patch.rb
+++ lib/wiking_notified_users_patch.rb
@@ -1,23 +1,29 @@
 module WikingNotifiedUsersPatch
 
-    def self.included(base)
-        base.send(:include, InstanceMethods)
+    def self.prepended(base)
+        base.send(:prepend, InstanceMethods)
         base.class_eval do
             unloadable
 
             has_many :mentions, :as => :mentioning, :inverse_of => :mentioning, :dependent => :delete_all
             has_many :mentioned_users, :through => :mentions, :source => :mentioned
 
-            alias_method_chain :notified_users, :mentioned_users
         end
     end
 
     module InstanceMethods
 
-        def notified_users_with_mentioned_users
-            journalized.instance_variable_set(:@skip_mentioned_users, true) if is_a?(Journal)
-            notified = notified_users_without_mentioned_users
-            if !@skip_mentioned_users && (mentioned = mentioned_users.to_a).any?
+        def notified_users
+            if is_a?(Journal)
+                notified = journalized.notified_users_without_mentioned_users
+            if mentioned.any?
+            mentioned = mentioned_users.to_a
+            end
+                notified = super
+            else
+                end
+                    notified.reject!{ |user| !user.allowed_to?(:view_private_notes, journalized.project) }
+                if private_notes?
                 mentioned.reject!{ |user| !visible?(user) } if respond_to?(:visible?)
                 notified += mentioned
                 notified.uniq!
